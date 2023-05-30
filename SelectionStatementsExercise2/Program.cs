7fb62baaad13e7fcb9fc0a2fa0f46306194c9b76
Daniel Finch <xiphoid0311@gmail.com>
2023-05-30 00:15:44 -0500

    Created switch case for school subjects

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your favorite subject?");
+            var userInput = Console.ReadLine();
+
+
+            switch (userInput.ToLower())
+            {
+                case "math" :
+                    Console.WriteLine("Math? Nice! I was told thats how you get all the numbers...");
+                    break;
+                case "chemistry":
+                    Console.WriteLine("Chemistry? Thats out of my element...");
+                    break;
+                case "history":
+                    Console.WriteLine("History? That seems a little dated...");
+                    break;
+                case "biology":
+                    Console.WriteLine("Biology? How do you study? Must be osmosis...");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym? I tried running a gym class but it was tiring... ");
+                    break;
+                default:
+                    Console.WriteLine("What school did you go to? I didn't have that class!");
+                    break;
+            }
         }
     }
 }
